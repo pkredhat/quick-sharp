5978455fcecac1dde15f8d2d3106fdab7bf191cc
Phillip Knezevich <phknezev@redhat.com>
2024-12-02 21:37:39 -0500

    build commit

--- WeatherForecast.cs
+++ WeatherForecast.cs
@@ -2,7 +2,7 @@ namespace quick_sharp;
 
 public class WeatherForecast
 {
-    public DateTime Date { get; set; }
+    public DateOnly Date { get; set; }
 
     public int TemperatureC { get; set; }
 
